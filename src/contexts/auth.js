46e9d6855a20f1051ca8fd2c32e51c59f2c16658
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-02-19 18:53:21 -0300

    feat: :sparkles: Login de usuário

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -28,10 +28,39 @@ function AuthProvider({ children }) {
             console.log(err)
             setLoadingAuth(false)
         }
-    
+    }
+
+    async function signIn(email, password) {
+        setLoadingAuth(true)
+        console.log('Email teste', email)
+        console.log('Senha teste', password)
+        try {
+            const response = await api.post('/login', {
+                email: email,
+                password: password
+            })
+            const { id, name, token } = response.data;
+            const data = {
+                id,
+                name,
+                token,
+                email
+            };
+            api.defaults.headers['Authorization'] = `Bearer ${token}`;
+            setUser({
+                id,
+                name,
+                email
+            });
+            setLoadingAuth(false)
+        } catch (err) {
+            console.error('Error ao logar')
+            console.log(err)
+            setLoadingAuth(false)
+        }
     }
     return (
-        <AuthContext.Provider value={{signed: !!user, user, signUp, loadingAuth}}>
+        <AuthContext.Provider value={{signed: !!user, user, signUp,signIn, loadingAuth}}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import { View, Text, Platform } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { View, Text, Platform, ActivityIndicator } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 // import { Container } from './styles';
 import { Background, Container, Logo, AreaInput, Input, SubmitButton, SubmitText, Link, LinkText } from './styles';
 import { useNavigation } from '@react-navigation/native';
+import { AuthContext } from '../../contexts/auth';
 
 export default function SignIn() {
     const navigation = useNavigation();
+    const { signIn, loadingAuth } = useContext(AuthContext)
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleLogin() {
+        signIn(email, password);
+    }
     return (
         <Background>
             <Container
@@ -20,15 +28,23 @@ export default function SignIn() {
                     <AreaInput>
                         <Input
                             placeholder={"Seu email"}
+                            value={email}
+                            onChangeText={(text) => setEmail(text)}
                         />
                     </AreaInput>
                     <AreaInput>
                         <Input
                             placeholder={"Sua senha"}
+                            value={password}
+                            onChangeText={(text) => setPassword(text)}
                         />
                 </AreaInput>
-                    <SubmitButton activeOpacity={0.8}>
-                        <SubmitText>Acessar</SubmitText>
+                <SubmitButton activeOpacity={0.8} onPress={handleLogin}>
+                    {
+                        loadingAuth ? <ActivityIndicator size={20} color={'#FFF'} /> : (
+                            <SubmitText>Acessar</SubmitText>
+                        )
+                    }
                 </SubmitButton>
                 <Link onPress={()=> navigation.navigate('SignUp')}>
                     <LinkText>
