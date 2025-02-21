02e672aa550fb2ab31e8425aa612093ad10e7289
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2025-02-21 16:17:42 -0300

    feat: :sparkles: Criando cards de movimentação

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,13 +4,50 @@ import { View, Text, Button } from "react-native"
 import { AuthContext } from "../../contexts/auth";
 
 import Header from '../../components/Header';
-import { Background } from "./styles";
+import { Background, ListaBalance } from "./styles";
+
+import api from "../../services/api";
+import { format } from "date-fns";
+import { useIsFocused } from "@react-navigation/native";
+import BalanceItem from "../../components/BalanceItem";
+
 export default function Home() {
     const { signOut, user } = useContext(AuthContext)
+    const isFocused = useIsFocused();
     const [listBalance, setListBalance] = useState([]);
+    const [dateMovements, setDateMovements] = useState(new Date())
+
+    useEffect(() => {
+        let isActivate = true;
+
+        async function getMovements() {
+            let dateFormated = format(dateMovements, 'dd/MM/yyyy')
+
+            const balance = await api.get('/balance', {
+                params: {
+                    date: dateFormated
+                }
+            })
+            if (isActivate) {
+                setListBalance(balance.data)
+            }
+        }
+        getMovements();
+
+        return () => isActivate = false;
+    }, [isFocused])
     return (
         <Background>
             <Header title={'Minhas movimentações'} />
+            <ListaBalance
+                data={listBalance}
+                horizontal={true}
+                showsHorizontalScrollIndcator={false}
+                keyExtractor={item => item.tag}
+                renderItem={({ item }) => (
+                    <BalanceItem data={item} />
+                ) }
+            />
         </Background>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,3 +4,7 @@ export const Background = styled.SafeAreaView`
 `;
 export const Title = styled.Text``;
 export const ButtonMenu = styled.TouchableOpacity``;
+export const ListaBalance = styled.FlatList`
+    max-height: 190px;
+    height: 190px;
+`
